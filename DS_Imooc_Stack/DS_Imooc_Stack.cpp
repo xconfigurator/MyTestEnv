83524eed77b46b73447df01eff6d761c3408e3a6
xconfigurator <xconfigurator@hotmail.com>
2021-11-19 15:48:22 +0800

    imooc的stack实现是非常别扭的，不要深究其API了，可以直接卡Larry版本的，API还是遵从STL的。

--- DS_Imooc_Stack/DS_Imooc_Stack.cpp
+++ DS_Imooc_Stack/DS_Imooc_Stack.cpp
@@ -1,6 +1,9 @@
 ﻿/*
 @since 2016/12/16
 @Author liuyang
+
+先说结论：imooc的这个stack实现非常别扭。不需要再看了，还是直接用stl的吧。
+
 栈
 字符串示例：[()]    [()()]    [()[()]]    [[()]	[()]]
 编译环境：Visual Studio 2015 Community
@@ -15,35 +18,38 @@ int main() {
 	MyStack<char>* pNeedStack = new MyStack<char>(100);// 急需的字符
 
 	//char str[] = "[()]";
-	char str[] = "[()()]";
+	//char str[] = "[()()]";
 	//char str[] = "[()[()]]";
 	//char str[] = "[[()]";
-	//char str[] = "[()]]";// 仅判栈空是有问题的。
+	char str[] = "[()]]";// 在imooc的栈实现中，仅判栈空是有问题的。通过swith中的语句在修复。这个就很别扭了。所以放弃imooc的这个stack的实现吧。看stl的或者是larry的。
 
 	char currentNeed = 0;// 当前需要的字符，储值给一个不可见字符的ASCII值。
 
 	for (int i = 0; i < strlen(str); i++) {
 		if (str[i] != currentNeed) {// 读到的不是急需的
 			pStack->push(str[i]);
-			if (currentNeed != 0) {
-				pNeedStack->push(currentNeed);
-			}
 			switch (str[i]) {
 			case '[':
+				if (currentNeed != 0) {
+					pNeedStack->push(currentNeed);
+				}
 				currentNeed = ']';
 				break;
 			case '(':
+				if (currentNeed != 0) {
+					pNeedStack->push(currentNeed);
+				}
 				currentNeed = ')';
 				break;
-			default:
-				cout << "字符串不匹配。" << endl;
+			default:// fix 
+				cout << "字符串括号不匹配" << endl;
 				return 0;
 			}
 		}
 		else {// 读到的就是需要的
 			char elem;
 			pStack->pop(elem);
-			if (!pNeedStack->pop(currentNeed)) {
+			if (!pNeedStack->pop(currentNeed)) {// fix 如果出栈不正常。
 				currentNeed = 0;
 			}
 		}
